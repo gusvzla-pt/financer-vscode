229d58e2c188ef17df9b6cc89ce5889ae42c77b7
gusoliv <gustavodiaz@ua.pt>
2025-05-02 11:28:56 +0100

    internationalization and routing fixed

--- i18n/request.js
+++ i18n/request.js
@@ -1,11 +1,27 @@
-import { getRequestConfig } from "next-intl/server";
-import { notFound } from "next/navigation";
-import { locales } from "../config";
+// import { getRequestConfig } from "next-intl/server";
+// import { notFound } from "next/navigation";
+// import { locales } from "../config";
 
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale)) notFound();
+// export default getRequestConfig(async ({ locale }) => {
+//   if (!locales.includes(locale)) notFound();
+
+//   return {
+//     messages: (await import(`../messages/${locale}.json`)).default,
+//   };
+// });
+
+import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "next-intl";
+import { routing } from "./routing";
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
 
   return {
+    locale,
     messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
